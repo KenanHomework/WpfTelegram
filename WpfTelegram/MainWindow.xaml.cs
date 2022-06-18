7693cb4bace6ac7b8f053b432a5922fc0343122e
kenan <86625368+kenanysbv@users.noreply.github.com>
2022-06-19 03:05:38 +0400

    start message menu

--- WpfTelegram/MainWindow.xaml.cs
+++ WpfTelegram/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
+using WpfTelegram.Models;
 using System.Windows.Shapes;
 
 namespace WpfTelegram
@@ -24,5 +26,6 @@ namespace WpfTelegram
         {
             InitializeComponent();
         }
+        public ObservableCollection<Contact> Contacts { get; set; }
     }
 }
---
+++ WpfTelegram/Models/Message.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfTelegram.Models
+{
+    public class Message
+    {
+        public Message(string text, bool writedByContact)
+        {
+            Text = text;
+            WritedByContact = writedByContact;
+        }
+
+        public string Text { get; set; }
+
+        public bool WritedByContact { get; set; }
+
+        public override string ToString() => Text;
+    }
+}
